4fa0f074238498b345e607c143ded513183e6f02
bod <1360799362@qq.com>
2019-06-07 00:27:56 +0800

    大量Demo 验证猜想  Barrier

---
+++ app/src/main/java/com/coroutine/bod/shanghai_jounery/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.coroutine.bod.shanghai_jounery
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_constraint)
+    }
+}
